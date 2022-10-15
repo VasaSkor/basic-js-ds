c5e8c6c1734de84fab336427c7f19393e0e38b14
VasaSkor <dzho.ker.700@gmail.com>
2022-10-15 17:19:04 +0300

    feat: add complete task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,14 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
-
+let _ = new ListNode(0)
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
  *
- * @param {List} l
+ * @param {ListNode} l
  * @param {Number} k
- * @return {List}
+ * @return {ListNode}
  *
  * @example
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value !== k)
+      getLastElement(_).next = new ListNode(l.value)
+
+  if (l.next !== null) {
+      return removeKFromList(l.next, k)
+  }
+  else {
+    const result = Object.assign({}, _.next)
+    _ = new ListNode(0)
+    return result
+  }
+}
+
+function getLastElement(value){
+  if(value.next !== null){
+    return getLastElement(value.next)
+  }
+  else{
+    return value
+  }
 }
 
 module.exports = {
